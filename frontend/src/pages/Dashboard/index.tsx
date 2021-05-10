48481aaa7da91bbb53cc104f1d2f70fd80e226f3
unknown <lucianagmbw@gmail.com>
2021-05-09 21:42:30 -0300

    modify text

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -14,7 +14,7 @@ const Dashboard = () => {
                 <div className="row px-3">
 
                     <div className="col-sm-6">
-                        <h5>Todas vendas</h5>
+                        <h5 className= "text-center text-secondary">Taxa de sucesso (%)</h5>
                         <BarChat />
                     </div>
 
@@ -22,10 +22,10 @@ const Dashboard = () => {
                         <h5>Todas vendas</h5>
                         <DonutChart />
                     </div>
-
-
                 </div>
-
+                <div className="py-3">
+                    <h2 className="text-primary">Todas Vendas </h2>
+                </div>
 
                 <DataTable />
             </div>
